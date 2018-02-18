d57b3b68b0267ffcc5cd3b763fe9436c55dcd848
Ollisteka <zhukova.o.m@yandex.ru>
2018-02-18 20:44:56 +0500

    переработка

---
+++ Lying_NTP/NtpServer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Lying_NTP
+{
+	internal class NtpServer
+	{
+		private readonly int offset;
+
+		public NtpServer(int offset=0)
+		{
+			this.offset = offset;
+		}
+
+		public int Port => 123;
+		private string serverAddress => "time.windows.com";
+
+		private DateTime AskServer(string serverName)
+		{
+			var ntpData = new byte[48];
+			/* Leap Indicator(LI) = 0 (no warning) 2 bit  (missing second)
+			 Version Number (VN) NTP/SNTP version number = 4 3 bit
+			 Mode = 3 (client) 3 bit */
+			ntpData[0] = 0b00100011; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
+
+			var addresses = Dns.GetHostEntry(serverName).AddressList;
+
+			var ipEndPoint = new IPEndPoint(addresses[0], Port);
+
+			using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
+			{
+				socket.Connect(ipEndPoint);
+
+				socket.ReceiveTimeout = 3000;
+
+				socket.Send(ntpData);
+				socket.Receive(ntpData);
+				socket.Close();
+			}
+
+			var transmitTimestamp = GetTimestamp(ntpData, 40);
+
+			var networkDateTime = new DateTime(1900, 1, 1, 0, 0, 0).AddSeconds((long) transmitTimestamp);
+			return networkDateTime.ToLocalTime();
+		}
+
+		public ulong GetTimestamp(byte[] data, int startIndex)
+		{
+			//Seconds
+			ulong intPart = BitConverter.ToUInt32(data, startIndex);
+
+			//Seconds fraction (random, so I won't bother) 
+			// ulong fractPart = BitConverter.ToUInt32(data, startIndex + 4);
+
+			//Convert From big-endian to little-endian
+			intPart = SwapEndianness(intPart);
+
+			return intPart;
+		}
+
+		public DateTime GetCurrentTime()
+		{
+			var date = AskServer(serverAddress);
+			return date.AddSeconds(offset);
+		}
+
+		private static uint SwapEndianness(ulong x)
+		{
+			return (uint) (((x & 0x000000ff) << 24) |
+							((x & 0x0000ff00) << 8) |
+							((x & 0x00ff0000) >> 8) |
+							((x & 0xff000000) >> 24));
+		}
+
+		private static int SwapEndianness(byte val)
+		{
+			return ((val & 0xF) << 4)
+					| ((val >> 4) & 0xF);
+		}
+
+		public void Run()
+		{
+			var data = new byte[1024];
+			var ipep = new IPEndPoint(IPAddress.Loopback, Port);
+			var newsock = new UdpClient(ipep);
+			while (true)
+			{
+				var sender = new IPEndPoint(IPAddress.Any, 0);
+				Console.WriteLine("Waiting for a client...");
+				data = newsock.Receive(ref sender);
+				var mode = data[0] & 0b00000111;
+				if (mode == 3)
+				{
+					Console.WriteLine("Message received from {0}, sending response...\n", sender);
+					data = Encoding.ASCII.GetBytes(GetCurrentTime().ToString(CultureInfo.InvariantCulture));
+					newsock.Send(data, data.Length, sender);
+				}
+				else
+				{
+					Console.WriteLine("Message received from {0} was incorrect:", sender);
+					Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
+					Console.WriteLine();
+				}
+			}
+		}
+	}
+}
--- Lying_NTP/Program.cs
+++ Lying_NTP/Program.cs
@@ -1,116 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Reflection;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using DocoptNet;
 
 namespace Lying_NTP
 {
-	class NtpClient
+	internal class Program
 	{
-		public const int NtpPort = 123;
+		private const string Usage = @"Lying NTP Server. Adds specified number of seconds to correct time.
 
-		public static DateTime GetNetworkTime()
+	Usage:
+	  Lying_NTP.exe
+	  Lying_NTP.exe [-n NUM | --num=NUM]
+	  Lying_NTP.exe (-h | --help)
+
+	Options:
+	  -n NUM --num=NUM   Specify how many seconds to add [default: 0]
+	  -h --help          Show this screen.
+
+	";
+
+		private static void Main(string[] args)
 		{
-			//default Windows time server
-			const string ntpServer = "time.windows.com";
+			var arguments = new Docopt().Apply(Usage, args, optionsFirst: true, exit: true);
 
-			// NTP message size - 16 bytes of the digest (RFC 2030)
-			var ntpData = new byte[48];
+			var offset = arguments["--num"].AsInt;
 
-			//Setting the Leap Indicator, Version Number and Mode values
-			ntpData[0] = 0x1B; //LI = 0 (no warning), VN = 3 (IPv4 only), Mode = 3 (Client Mode)
-
-			var addresses = Dns.GetHostEntry(ntpServer).AddressList;
-
-			//The UDP port number assigned to NTP is 123
-			var ipEndPoint = new IPEndPoint(addresses[0], NtpPort);
-			//NTP uses UDP
-
-			using (var socket = new Socket(AddressFamily.InterNetwork,
-				SocketType.Dgram,
-				ProtocolType.Udp))
-			{
-				socket.Connect(ipEndPoint);
-
-				//Stops code hang if NTP is blocked
-				socket.ReceiveTimeout = 3000;
-
-				socket.Send(ntpData);
-				socket.Receive(ntpData);
-				socket.Close();
-			}
-
-			//Offset to get to the "Transmit Timestamp" field (time at which the reply 
-			//departed the server for the client, in 64-bit timestamp format."
-			const byte serverReplyTime = 40;
-
-			//Get the seconds part
-			ulong intPart = BitConverter.ToUInt32(ntpData, serverReplyTime);
-
-			//Get the seconds fraction
-			ulong fractPart = BitConverter.ToUInt32(ntpData, serverReplyTime + 4);
-
-			//Convert From big-endian to little-endian
-			intPart = SwapEndianness(intPart);
-			fractPart = SwapEndianness(fractPart);
-
-			var milliseconds = (intPart * 1000) + ((fractPart * 1000) / 0x100000000L);
-
-			//**UTC** time
-			var networkDateTime = (new DateTime(1900, 1, 1, 0, 0, 0, DateTimeKind.Utc)).AddMilliseconds((long) milliseconds);
-
-			return networkDateTime.ToLocalTime();
-		}
-
-		// stackoverflow.com/a/3294698/162671
-
-		private static uint SwapEndianness(ulong x)
-		{
-			return (uint) (((x & 0x000000ff) << 24) +
-							((x & 0x0000ff00) << 8) +
-							((x & 0x00ff0000) >> 8) +
-							((x & 0xff000000) >> 24));
-		}
-	}
-
-	class Program
-	{
-		static void Main(string[] args)
-		{
+			var server = new NtpServer(offset);
 			Console.WriteLine("\nPress ESC to exit");
-			Console.WriteLine("Press Enter to learn time");
-			byte[] data = new byte[1024];
-			IPEndPoint ipep = new IPEndPoint(IPAddress.Loopback, NtpClient.NtpPort);
-			UdpClient newsock = new UdpClient(ipep);
+			Task.Run(() => Quit());
+			Task.Run(() => RunClient());
+			server.Run();
+			
+		}
+
+		private static void RunClient()
+		{
 			while (true)
 			{
-				IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
-
-				Console.WriteLine("Waiting for a client...");
-				data = newsock.Receive(ref sender);
-				Console.WriteLine("Here");
-				Console.WriteLine("Message received from {0}:", sender.ToString());
-				Console.WriteLine(Encoding.ASCII.GetString(data, 0, data.Length));
-				string welcome = "Welcome to my test server";
-				data = Encoding.ASCII.GetBytes(welcome);
-				newsock.Send(data, data.Length, sender);
-				data = Encoding.ASCII.GetBytes(NtpClient.GetNetworkTime().ToString());
-				newsock.Send(data, data.Length, sender);
-
-				switch (Console.ReadKey(true).Key)
+				Thread.Sleep(1000);
+				var ntpData = new byte[48];
+				ntpData[0] = 3;
+				var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 123);
+				
+				using (var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp))
 				{
-					case ConsoleKey.Escape:
-						Environment.Exit(1);
-						break;
-					case ConsoleKey.Enter:
-						Console.WriteLine(NtpClient.GetNetworkTime());
-						break;
+					socket.Connect(ipEndPoint);
+
+					socket.ReceiveTimeout = 3000;
+
+					socket.Send(ntpData);
+					socket.Receive(ntpData);
+					socket.Close();
 				}
+				
+				Console.WriteLine(System.Text.Encoding.Default.GetString(ntpData));
 			}
+			
+		}
+
+		private static void Quit()
+		{
+			while (true)
+				if (Console.ReadKey().Key == ConsoleKey.Escape)
+					Environment.Exit(1);
 		}
 	}
 }
